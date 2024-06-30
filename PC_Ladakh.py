35455f6cf92532f364d45aa0880e1219d8701d15
Natasha Kumari <140797294+Natasha0206@users.noreply.github.com>
2024-06-30 16:55:28 +0530

    Add files via upload

---
+++ PC_Ladakh.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+req = requests.get("https://results.eci.gov.in/PcResultGenJune2024/partywiseresult-U09.htm")
+soup = BeautifulSoup(req.content,"html.parser")
+res = soup.get_text()
+text = " ".join(res.split())
+
+temp = ""
+for i in text:
+    if (i.isspace() or i.isalnum()==False):
+        print(temp)
+        temp = ""
+        continue
+    else:
+        temp+=i
